f47f84a2205ed51f8f983f93ead25433af15ea13
ezecastiglione18 <BocaVoley18>
2020-07-14 11:32:50 -0300

    Cambios en zonasLimitrofes/1. Ya modifique todo en el programa

--- tpLogico.pl
+++ tpLogico.pl
@@ -20,15 +20,10 @@ camino([comarca, rivendel, moria, lothlorien, edoras, minasTirith, minasMorgul, 
 %zonasLimitrofes(una zona, otra zona)
 
 zonasLimitrofes(rivendel, moria).
-zonasLimitrofes(moria, rivendel).
 zonasLimitrofes(moria, isengard).
-zonasLimitrofes(isengard, moria).
 zonasLimitrofes(lothlorien, edoras).
-zonasLimitrofes(edoras, lothlorien).
 zonasLimitrofes(edoras, minasTirith).
-zonasLimitrofes(minasTirith, edoras).
 zonasLimitrofes(minasTirith, minasMorgul).
-zonasLimitrofes(minasMorgul, minasTirith).
 
 zonasLimitrofes(Zona1, Zona2) :-
     mismaRegion(Zona1, Zona2), Zona1 \= Zona2.
@@ -36,61 +31,67 @@ zonasLimitrofes(Zona1, Zona2) :-
 mismaRegion(Zona1, Zona2) :-
     zona(Zona1, Region), zona(Zona2, Region).
 
+sonZonasLimitrofes(Zona1, Zona2):-
+    zonasLimitrofes(Zona1, Zona2).
+
+sonZonasLimitrofes(Zona1, Zona2):-
+    zonasLimitrofes(Zona2, Zona1).
+
 %Punto4
 
 regionesLimitrofes(UnaRegion,OtraRegion) :-
- zona(UnaZona,UnaRegion),
- zona(OtraZona,OtraRegion),
- zonasLimitrofes(UnaZona,OtraZona),
- UnaRegion \= OtraRegion.
+    zona(UnaZona,UnaRegion),
+    zona(OtraZona,OtraRegion),
+    sonZonasLimitrofes(UnaZona,OtraZona),
+    UnaRegion \= OtraRegion.
 
 regionesLejanas(UnaRegion,OtraRegion) :-
- zona(_,UnaRegion),
- zona(_,OtraRegion),
- not(regionesLimitrofes(UnaRegion,OtraRegion)),
- not(hayRegionLimitrofeEntre(UnaRegion,OtraRegion)).
+    zona(_,UnaRegion),
+    zona(_,OtraRegion),
+    not(regionesLimitrofes(UnaRegion,OtraRegion)),
+    not(hayRegionLimitrofeEntre(UnaRegion,OtraRegion)).
 
 hayRegionLimitrofeEntre(UnaRegion,OtraRegion) :-
- regionesLimitrofes(UnaRegion,UnaRegionIntermedia),
- regionesLimitrofes(OtraRegion,UnaRegionIntermedia).
+    regionesLimitrofes(UnaRegion,UnaRegionIntermedia),
+    regionesLimitrofes(OtraRegion,UnaRegionIntermedia).
 
 %Punto5
 
-puedeSeguirCon(UnCamino,UnaZona) :-
+puedeSeguirCon(UnCamino,UnaZona):-
     last(UnCamino,UltimaZona),
-    zonasLimitrofes(UnaZona,UltimaZona).
+    sonZonasLimitrofes(UnaZona,UltimaZona).
 
-sonConsecutivos(UnCamino,OtroCamino) :-
+sonConsecutivos(UnCamino,OtroCamino):-
     nth1(1,OtroCamino,UnaZona),
     puedeSeguirCon(UnCamino,UnaZona).
 
 % Punto 6
 
-tieneLogica([Zona|ZonaSiguiente|RestoDeZonas]) :-
-    zonasLimitrofes(Zona, ZonaSiguiente),
+tieneLogica([Zona|ZonaSiguiente|RestoDeZonas]):-
+    sonZonasLimitrofes(Zona, ZonaSiguiente),
     tieneLogica([ZonaSiguiente|RestoDeZonas]).
 
-esSeguro([Zona|ZonaSiguiente|RestoDeZonas]) :-
+esSeguro([Zona|ZonaSiguiente|RestoDeZonas]):-
     cambiaDeRegion(Zona, ZonaSiguiente),
     esSeguro(RestoDeZonas).
 
-esSeguro([_|Zona|ZonaSiguiente|RestoDeZonas]) :-
+esSeguro([_|Zona|ZonaSiguiente|RestoDeZonas]):-
     cambiaDeRegion(Zona, ZonaSiguiente),
     esSeguro(RestoDeZonas).
 
-cambiaDeRegion(UnaZona, OtraZona) :-
+cambiaDeRegion(UnaZona, OtraZona):-
     zona(UnaZona,UnaRegion),
     zona(OtraZona,OtraRegion),
     UnaRegion \= OtraRegion
 
 % Punto 7
 
-cantidadDeRegiones(Camino, CantidadDeRegiones) :-
+cantidadDeRegiones(Camino, CantidadDeRegiones):-
     findall(Region, regionDe(Camino, Region), Regiones), % Tengo que conseguir una lista de regiones
     list_to_set(Regiones, RegionesSinRepetidos),
     length(RegionesSinRepetidos, CantidadDeRegiones).
 
- todosLosCaminosConducenAMordor(Camino|Resto) :-
+ todosLosCaminosConducenAMordor(Camino|Resto):-
     last(Camino, UltimaZona),
     zona(UltimaZona, mordor),
     todosLosCaminosConducenAMordor(Resto).

a36b33de82a426ba31fd15c6c1442ff6197d6f0b
Ignacio-Ferrante <ignacioferrante1379@gmail.com>
2020-07-18 19:45:17 -0300

    punto 10 y 11

--- tpLogico.pl
+++ tpLogico.pl
@@ -137,6 +137,9 @@ raza(Nombre, Tipo) :-
     tipoDeRaza(Raza, Tipo).
 
 tipoDeRaza(guerrero(_), guerrera).
+tipoDeRaza(guerrero(elfo),   elfo).
+tipoDeRaza(guerrero(enano),   enano).
+tipoDeRaza(guerrero(dunedain),   dunedain).
 tipoDeRaza(maiar(_,_),  maiar).
 tipoDeRaza(pacifista(_),   pacifista).
 
@@ -169,3 +172,65 @@ nivelDelArma(arco(Nivel), Nivel).
 nivelDelArma(hacha(Nivel), Nivel).
 nivelDelArma(espada(Nivel), Nivel).
 nivelDelArma(ballesta(Nivel), Nivel).
+
+% Punto 10
+
+requisitosDeRaza(_,ListaDeViajeros,NivelRaza,TipoRaza) :-
+ member(Viajero,ListaDeViajeros),
+ raza(Viajero,TipoRaza),
+ nivel(Viajero,Nivel),
+ Nivel >= NivelRaza.
+
+requisitosDeElementos(_,ListaDeViajeros,ArmaRequerida,CantidadRequerida) :-
+ findall(ArmaRequerida,tienenArma(ListaDeViajeros,ArmaRequerida),ArmasViajeros),
+ length(ArmasViajeros,CantidadDeArmas),
+ CantidadDeArmas >= CantidadRequerida.
+
+tienenArma(ListaDeViajeros,Arma) :-
+ member(Viajero,ListaDeViajeros),
+ tieneArma(Viajero,Arma).
+
+tieneArma(Viajero,Arma) :-
+ viajero(Viajero,_,armas(ListaDeArmas)),
+ member(Arma,ListaDeArmas).
+
+
+requisitosDeMagia(_,ListaDeViajeros,PoderMagicoMinimo) :-
+ findall(PoderMagico,poderes(ListaDeViajeros,PoderMagico),PoderesMagicos),
+ sumlist(PoderesMagicos,SumaPoderMagico),
+ SumaPoderMagico >= PoderMagicoMinimo.
+
+poderes(ListaDeViajeros,PoderMagico) :-
+ member(Viajero,ListaDeViajeros),
+ poder(Viajero,PoderMagico).
+
+poder(Viajero,PoderMagico) :-
+ raza(Viajero,elfo),
+ nivel(Viajero,Nivel),
+ PoderMagico is Nivel*2.
+poder(Viajero,Nivel) :-
+ raza(Viajero,enano),
+ nivel(Viajero,Nivel).
+poder(Viajero,Nivel) :-
+ raza(Viajero,dunedain),
+ nivel(Viajero,Nivel).
+
+puedeAtravesar(moria,ListaDeViajeros) :-
+ requisitosDeRaza(moria,ListaDeViajeros,24,maiar).
+puedeAtravesar(isengard,ListaDeViajeros) :-
+ requisitosDeRaza(isengard,ListaDeViajeros,27,maiar).
+puedeAtravesar(isengard,ListaDeViajeros) :-
+ requisitosDeRaza(isengard,ListaDeViajeros,30,elfo).
+puedeAtravesar(moria,ListaDeViajeros) :-
+ requisitosDeElementos(moria,ListaDeViajeros,cotaDeMalla,1).
+puedeAtravesar(moria,ListaDeViajeros) :-
+ requisitosDeElementos(moria,ListaDeViajeros,panDeLembas,2).
+%puedeAtravesar(Zona,ListaDeViajeros) :-
+ %requisitosDeRaza(Zona,ListaDeViajeros,_,_),
+ %requisitosDeElementos(Zona,ListaDeViajeros,_,_),
+ %requisitosDeMagia(Zona,ListaDeViajeros,_).
+
+% Punto 11
+seSienteComoEnCasa(ListaDeViajeros,Region) :-
+ zona(_,Region),
+ forall(zona(Zona,Region),puedeAtravesar(Zona,ListaDeViajeros)).
